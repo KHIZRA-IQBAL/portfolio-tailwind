baa0a32c8f94729b75a286d295911f16e28d42e3
KHIZRA-IQBAL <iqbalkhizra026@gmail.com>
2024-11-10 18:13:22 +0500

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <footer className="bg-[#dfd0bb] text-[#202124] py-6 text-center text-md lg:text-2xl font-[Caveat]">© 2024 Khizra Iqbal. All rights reserved.</footer>
+    </>
+  )
+}
+
+export default Footer
